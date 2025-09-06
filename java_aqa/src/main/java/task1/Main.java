ab6aabdb3757be6bc24ee0c84398a8442c933fb6
Markiyan <goro100100@gmail.com>
2025-09-06 12:23:49 +0300

    task1 done

---
+++ java_aqa/src/main/java/task1/Main.java
@@ -0,0 +1,18 @@
+package task1;
+
+public class Main {
+//V 8: StringBuilder
+//Objective: To understand and use the StringBuilder class in Java.
+//Instructions:
+//Use the StringBuilder class to build a string by appending characters.
+//Print the result to the console.
+
+    public static void main(String[] args) {
+        StringBuilder stringbuilder = new StringBuilder();
+
+        for (char i = 'a'; i <= 'z'; i++){
+            System.out.println(i);
+
+        }
+    }
+}
